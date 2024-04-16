a491e03f0c6b10139611fa40c2248f09b70473c1
Vyacheslav <stormfall228@gmai.com>
2024-04-16 12:43:46 +0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const { mongoose } = require("mongoose");
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("Database not connected", err));
+
+const app = express();
+
+app.use("/", require("./routes/authRoutes"));
+
+const port = 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
